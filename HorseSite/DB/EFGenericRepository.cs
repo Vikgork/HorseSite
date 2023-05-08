4bc1f6cd0a2dd586c713caba9e21ad40ab0f7cdf
Viktor <vgorkutsenko@gmail.com>
2023-05-08 13:28:30 +0300

    Add db(test, sqllite)

---
+++ HorseSite/DB/EFGenericRepository.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HorseSite.DB
+{
+    public class EFGenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        DbContext _context;
+        DbSet<TEntity> _dbSet;
+
+        public EFGenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return _dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+        public TEntity Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            try
+            {
+                return _dbSet.FirstOrDefault(predicate);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public TEntity FindById(int id)
+        {
+            try
+            {
+                return _dbSet.Find(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public void Create(TEntity item)
+        {
+            try
+            {
+                _dbSet.Add(item);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Update(TEntity item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public void Remove(TEntity item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+        public IEnumerable<TEntity> GetWithInclude(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            return Include(includeProperties).ToList();
+        }
+
+        public IEnumerable<TEntity> GetWithInclude(Func<TEntity, bool> predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var query = Include(includeProperties);
+            return query.Where(predicate).ToList();
+        }
+
+        private IQueryable<TEntity> Include(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbSet.AsNoTracking();
+            return includeProperties
+                .Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ HorseSite/DB/MainContext.cs
@@ -0,0 +1,16 @@
+﻿using HorseSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HorseSite.DB
+{
+    public class MainContext :DbContext
+    {
+        public DbSet<HorseShort> Horses => Set<HorseShort>();
+        public MainContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=horsesiteDB.db");
+        }
+    }
+}
